49a08fc4ab16c5a266d27d949225bdf04f081568
Ning Ma <ningma1997@gmail.com>
2022-03-25 15:47:39 +0800

    Initial Commit

---
+++ Shared/HyperClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperClientApp.swift
+//  Shared
+//
+//  Created by Niko Ma on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct HyperClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
